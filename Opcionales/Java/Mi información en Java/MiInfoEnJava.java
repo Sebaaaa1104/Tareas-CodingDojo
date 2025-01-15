d6fa1be6ce29e9dcde2e995db616f83a6ff2fbdd
Sebaaaa1104 <sebastiantapiacaceres@gmail.com>
2025-01-15 16:41:00 -0300

    Empezo java

---
+++ Opcionales/Java/Mi información en Java/MiInfoEnJava.java
@@ -0,0 +1,8 @@
+public class MiInfoEnJava {
+    public static void main(String [] args){
+        System.out.println("Hola, mi nombre es Sebastian Tapia.");
+        System.out.println("Tengo 23 años de edad.");
+        System.out.println("Soy de Chile y vivo en la ciudad de Santiago.");
+        System.out.println("Me gusta mucho programar, hacer ejercicio y los videojuegos.");
+    }
+}
